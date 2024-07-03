547e5f6cc6804781f4c26f45575c30ddf0720bbc
rbides <renanbides@gmail.com>
2024-07-03 19:02:18 -0300

    add logging

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
+import logging
 from fastapi import FastAPI
 
 
 app = FastAPI()
-
+logging.basicConfig(level=logging.DEBUG)
 
 @app.get("/")
 def hello_world():
+    logging.debug("Entering hello_world function.")
     return "Hello World!"
 
